23520d6c5a78823d101cb8852129696e09d608e3
Vibhu Vanjari <vibhu.vanjari@kellogg.northwestern.edu>
2025-02-04 22:24:05 +0000

    completed code-along 3-associations

--- code-along/3-associations.rb
+++ code-along/3-associations.rb
@@ -10,8 +10,34 @@ Contact.destroy_all
 
 # - Insert and read contact data for companies in the database
 
+puts "Contacts: #{Contact.all.count}"
+
 # 1. insert new rows in the contacts table with relationship to a company
+
+apple = Company.find_by({"name" => "Apple"})
+puts apple.inspect
+
+contact = Contact.new
+contact["first_name"] = "Tim"
+contact["last_name"] = "Cook"
+contact["email"] = "tim@apple.com"
+contact["company_id"] = apple["id"]
+contact.save
+puts contact.inspect
+
+puts "Contacts: #{Contact.all.count}"
 
 # 2. How many contacts work at Apple?
 
+apple_contacts = Contact.where({"company_id" => apple["id"]})
+puts "Apple people: #{apple_contacts.count}"
+
 # 3. What is the full name of each contact who works at Apple?
+
+contacts = Contact.where({"company_id" => apple["id"]})
+for contact in contacts
+    first_name = contact["first_name"]
+    last_name = contact["last_name"]
+    name = "#{first_name} #{last_name}"
+    puts name
+end
